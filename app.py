24e0cf85551d36f245b7ac3c6e390086e549bfbf
DRRMAST <heztzxy@yeah.net>
2020-06-01 23:53:04 +0800

    Add base template, error template and IMDb link

--- app.py
+++ app.py
@@ -4,20 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import click
 
-name = 'Zhitong He'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
@@ -56,17 +42,22 @@ def initdb(drop):
 	db.create_all()
 	click.echo('Initialized database')
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def index():
-	user = User.query.first()
-	movies = Movie.query.all()
-	return render_template('index.html', name=name, movies=movies)
-def hello():
-	return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/index')
-@app.route('/home')
-
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+    
 @app.route('/user/<name>')
 def user_page(name):
 	return 'User: %s' % escape(name)
